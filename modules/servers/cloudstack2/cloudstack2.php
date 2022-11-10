20c62b1aba044156a02e92073ec6826a73f7f5b4
Milen Hristov <m.hristov@executie.org>
2022-11-10 21:54:10 +0000

    commit

--- modules/servers/cloudstack2/cloudstack2.php
+++ modules/servers/cloudstack2/cloudstack2.php
@@ -76,7 +76,7 @@ function cloudstack2_LoaderFunction() {
             'cloudstack2',
             __FUNCTION__,
             $i,
-            $template);
+            $template[$i]['id']);
             $list[[$i]['id']] = ucfirst($template['name']);
     }
     logModuleCall(
