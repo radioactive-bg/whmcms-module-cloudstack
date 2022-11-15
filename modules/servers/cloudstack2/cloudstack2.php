ecfa1215dc8a63d6d983cc91400588dee49f9969
Milen Hristov <m.hristov@executie.org>
2022-11-15 13:58:15 +0000

    commit

--- modules/servers/cloudstack2/cloudstack2.php
+++ modules/servers/cloudstack2/cloudstack2.php
@@ -272,7 +272,7 @@ function cloudstack2_CreateAccount(array $params) {
                             Capsule::table('mod_cloudstack2')->updateOrInsert(
                                 ['serviceId' => $params['serviceid']],
                                 [
-                                    'vmPassword' => $deploy_job_status['queryasyncjobresultresponse']['jobresult']['virtualmachine']['password'];
+                                    'vmPassword' => $deploy_job_status['queryasyncjobresultresponse']['jobresult']['virtualmachine']['password']
                                 ]
                                 );
                     }
