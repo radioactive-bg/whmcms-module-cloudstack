e0025f705513f2c1db90b9a6d5fa4c8c0634982a
Milen Hristov <m.hristov@executie.org>
2022-11-16 13:45:37 +0000

    commit

--- modules/servers/cloudstack2/cloudstack2.php
+++ modules/servers/cloudstack2/cloudstack2.php
@@ -603,9 +603,8 @@ function cloudstack2_AdminServicesTabFields(array $params)
         return array(
             'Server ID' => $server_stat->serverId,
             'Network ID' => $server_stat->networkId,
-            'PortForward TCP ID' => $server_stat->portForwardTcpId,
-            'PortForward UDP ID' => $server_stat->portForwardUdpId,
-            'PortForward ICMP ID' => $server_stat->portForwardIcmpId,
+            'PortForward TCP ID' => $server_stat->portforwardTCPId,
+            'PortForward UDP ID' => $server_stat->portforwardUDPId,
             'UDP Firewall ID' => $server_stat->firewallUDPId,
             'TCP Firewall ID' => $server_stat->firewallTCPId,
             'ICMP Firewall ID' => $server_stat->firewallICMPId,
